3f1ad9649b6e65899d880e0a2b2081136426d787
Liska <maxsroe@gmail.com>
2023-10-18 10:03:49 -0500

    hello window

--- StellarHavoc/StellarHavoc.cpp
+++ StellarHavoc/StellarHavoc.cpp
@@ -1,20 +1,38 @@
-// StellarHavoc.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulcan Window", nullptr, nullptr);
+
+    uint32_t extentionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+    std::cout << extentionCount << " extentions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
