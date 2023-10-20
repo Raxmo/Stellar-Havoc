2fd80f27600706813ca5df82f8e65001306d7d72
Liska <maxsroe@gmail.com>
2023-10-20 01:48:09 -0500

    added more boilerplate code

--- StellarHavoc/hed/shpipeline.hpp
+++ StellarHavoc/hed/shpipeline.hpp
@@ -5,16 +5,44 @@
 #include <fstream>
 #include <iostream>
 
+#include "shdevice.hpp"
+
 namespace stellarhavoc
 {
+	struct pipelineconfiginfo
+	{
+
+	};
+
 	class shpipeline
 	{
 	public:
-		shpipeline(const std::string vertPath, const std::string fragPath);
+		shpipeline(
+			shDevice &device, 
+			const std::string vertPath, 
+			const std::string fragPath, 
+			const pipelineconfiginfo &cfginfo);
+		~shpipeline() {}
+
+		shpipeline(const shpipeline&) = delete;
+		void operator=(const shpipeline&) = delete;
+
+		static pipelineconfiginfo createdefaultconfiginfo(uint32_t width, uint32_t height);
 
 	private:
 		static std::vector<char> readfile(const std::string& path);
 
-		void createPipeline(const std::string vertpath, const std::string fragpath);
+		void createPipeline(
+			const std::string vertpath, 
+			const std::string fragpath,
+			const pipelineconfiginfo cfginfo);
+
+		void createShaderModule(const std::vector<char>& code, VkShaderModule* shadermodule);
+
+		shDevice& device;
+		VkPipeline graphicsPipeline;
+		VkShaderModule vertmodule;
+		VkShaderModule fragmodule;
+
 	};
 }
--- StellarHavoc/src/shpipeline.cpp
+++ StellarHavoc/src/shpipeline.cpp
@@ -2,9 +2,18 @@
 
 namespace stellarhavoc
 {
-	shpipeline::shpipeline(const std::string vertPath, const std::string fragPath)
+	shpipeline::shpipeline(
+		shDevice& device,
+		const std::string vertPath,
+		const std::string fragPath,
+		const pipelineconfiginfo& cfginfo)
+		: device(device)
 	{
-		createPipeline(vertPath, fragPath);
+		createPipeline(vertPath, fragPath, cfginfo);
+	}
+	pipelineconfiginfo shpipeline::createdefaultconfiginfo(uint32_t width, uint32_t height)
+	{
+		return pipelineconfiginfo();
 	}
 	std::vector<char> shpipeline::readfile(const std::string& path)
 	{
@@ -26,12 +35,27 @@ namespace stellarhavoc
 		return buffer;
 	}
 
-	void shpipeline::createPipeline(const std::string vertpath, const std::string fragpath)
+	void shpipeline::createPipeline(
+		const std::string vertpath,
+		const std::string fragpath,
+		const pipelineconfiginfo cfginfo)
 	{
 		auto vertcode = readfile(vertpath);
 		auto fragcode = readfile(fragpath);
 
 		std::cout << "vertex cote size: " << vertcode.size() << "\n";
 		std::cout << "Fragment code size: " << fragcode.size() << "\n";
+	}
+	void shpipeline::createShaderModule(const std::vector<char>& code, VkShaderModule* shadermodule)
+	{
+		VkShaderModuleCreateInfo sminfo;
+		sminfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		sminfo.codeSize = code.size();
+		sminfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		if (vkCreateShaderModule(device.device(), &sminfo, nullptr, shadermodule) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create shader module");
+		}
 	}
 }
